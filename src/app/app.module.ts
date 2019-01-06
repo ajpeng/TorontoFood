bbc8c871a5a57c809a57c15ecc4ab0614dbe0756
craexus <jjpeng@ryerson.ca>
2019-01-06 18:58:39 -0500

    Added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AlertModule, BsDropdownModule, CarouselModule, CollapseModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { MainContentComponent } from './main/main-content/main-content.component';
+
 
 @NgModule({
   declarations: [
