33e9d14684eb7c7d18230607560830a61bc9832b
craexus <jjpeng@ryerson.ca>
2019-01-06 14:24:40 -0500

    added seperate directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule, BsDropdownModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { MainContentComponent } from './main/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
